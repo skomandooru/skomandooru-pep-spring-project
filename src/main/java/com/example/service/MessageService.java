87a976add7f5b0389f4d5f5f26ae8ade96c38246
Surya Komandooru <skomandooru@gmail.com>
2023-09-27 16:33:20 +0000

    tried something

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -14,32 +14,10 @@ public class MessageService {
     @Autowired 
     private MessageRepository messageRepository;
 
-  /*  public Message updateMessage (Message message) throws Exception {
-    
-    if(message.getMessage_text()==null || message.getMessage_text().equals("")){
-        throw new Exception("message text is empty");
-    } 
-
-    if(message.getMessage_text().length()>255) 
-        throw new Exception("too many characters");
-  
-    if(!messageRepository.existsById(message.getMessage_id()))
-        throw new Exception("message does not exist");
-
-    return messageRepository.save(message);
-    } 
-*/
     public List <Message> getAllMessages () {
         return messageRepository.findAll();
     }
     
- /*    public Message retrieveMessagebyid (Integer messageId) throws Exception{
-        if(!messageRepository.existsById(messageId))
-        throw new Exception("message does not exist");    
-    
-        return messageRepository.findById(messageId).get();
-    }
-*/
     public Message createmessage(Message message) throws Exception {
         if(message.getMessage_text()==null || message.getMessage_text().equals("")){
             throw new Exception("message text is empty");
